d03f547b4f06e4cfa793142019961fb6367527ff
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-26 02:34:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,7 +10,8 @@ end
 def countdown_with_sleep(num_of_secs)
   counter = num_of_secs
   while counter > 0 
-  puts "#{counter} SECOND(S)!"
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
     counter -= 1 
   end
   "HAPPY NEW YEAR!"
